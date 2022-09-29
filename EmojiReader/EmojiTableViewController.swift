52436c8192e464d5c740d22871484cd9b856f17a
AnnaSemichaeva <tdhnss@gmail.com>
2022-09-29 16:06:52 +0300

    minor changes

--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -28,6 +28,13 @@ class EmojiTableViewController: UITableViewController {
         
     }
     
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        
+    }
+    
+    
+    
+    
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
